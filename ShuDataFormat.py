bc5f39ace21228c7d6be44d882adec1a4d2bccac
Krish Ganotra <krishganotra@gmail.com>
2019-12-10 20:13:56 -0500

    Data formatted!!

---
+++ ShuDataFormat.py
@@ -0,0 +1,57 @@
+import json
+import os
+
+
+'''
+url
+text
+images
+top_img
+keywords
+authors
+canonical_link
+title
+meta_data
+movies
+publish_date
+source
+summary
+'''
+
+def importFolderIntoData(data, folder, count, label): #given current data dict, add data from all files in given folder into data dict
+    for subfolder in os.listdir(folder):
+        subfolderpath = folder + "/" + subfolder
+        for filename in os.listdir(subfolderpath):
+            with open(subfolderpath + "/" + filename) as jsonfile:
+                tempData = json.load(jsonfile)
+                if(len(tempData["text"]) <= 10): #if there is no text (or near no text), don't include datapoint
+                    pass
+                outText = "{url}\n\n{authors}\n\n{title}\n\n{text}\n\n{summary}".format(url=tempData["url"],
+                                                                                        authors=', '.join(tempData["authors"]),
+                                                                                        title=tempData["title"],
+                                                                                        text=tempData["text"],
+                                                                                        summary=tempData["summary"])
+                data["text"][str(count)] = outText
+                data["label"][str(count)] = label
+                count += 1
+
+    return data, count
+
+path = "./FakeNewsNet/code/fakenewsnet_dataset/"
+
+data = json.load(open("data.json")) # data = {"text":{"0":"...", "1":... etc.}, "label":{"0":1, "1", 0 etc.}}
+count = len(data["text"])
+
+
+
+print("starting politifact fake; count =", count)
+data, count = importFolderIntoData(data, path+"politifact/fake", count, 0)
+print("starting politifact real; count =", count)
+data, count = importFolderIntoData(data, path+"politifact/real", count, 1)
+print("starting gossipcop fake; count =", count)
+data, count = importFolderIntoData(data, path+"gossipcop/fake", count, 0)
+print("starting gossipcop real; count =", count)
+data, count = importFolderIntoData(data, path+"gossipcop/real", count, 1)
+
+with open('data.json', 'w', encoding="utf8") as outfile:
+    json.dump(data, outfile)
--- dataToJson.py
+++ dataToJson.py
@@ -7,18 +7,18 @@ count = 0
 
 for filename in os.listdir("./fake"):
     with open("./fake/" + filename, encoding='utf8') as fakeFile:
-        print(filename)
+        print(filename, "added to data")
         text = fakeFile.read().strip().replace("\n", " ")
         data["text"][str(count)] = text
-        data["label"][str(count)] = 0
+        data["label"][str(count)] = 0 #label 0 indicates fake
         count += 1
 
 for filename in os.listdir("./real"):
     with open("./real/" + filename, encoding='utf8') as realFile:
-        print(filename)
+        print(filename, "added to data")
         text = realFile.read().strip().replace("\n", " ")
         data["text"][str(count)] = text
-        data["label"][str(count)] = 1
+        data["label"][str(count)] = 1 #label 1 indicates real
         count += 1
 
 
