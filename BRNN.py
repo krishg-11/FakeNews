5e9f2e20ba63272daf1e6f23829f10156efcd5ad
Krish Ganotra <krishganotra@gmail.com>
2020-03-18 18:39:43 -0400

    optimal hyperparameters chosen

--- BRNN.py
+++ BRNN.py
@@ -37,14 +37,14 @@ print(X_test.shape,Y_test.shape)
 #make model
 model = Sequential()
 model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
-model.add(SpatialDropout1D(0.4))
-model.add(Bidirectional(LSTM(100, dropout=0.4, recurrent_dropout=0.4)))
+model.add(SpatialDropout1D(0.2))
+model.add(Bidirectional(LSTM(75, dropout=0.2, recurrent_dropout=0.2)))
 model.add(Dense(2, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adagrad', metrics=['accuracy'])
 
 #hyperparameters
-epochs = 3
-batch_size = 32
+epochs = 5
+batch_size = 64
 
 #train model
 history = model.fit(X_train, Y_train, epochs=epochs, batch_size=batch_size,validation_split=0.1,callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
